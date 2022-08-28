d301cfd0f49d9d9cf43090b024d343548772db0e
Andreas Remdt <me@andreasremdt.com>
2022-08-28 12:20:16 +0200

    feat: add `PostPreview` and `Image` components

---
+++ src/helpers.ts
@@ -0,0 +1,23 @@
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat("de-DE", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }).format(new Date(date));
+}
+
+export function getCategoryFromUrl(url: string) {
+  return url.split("/")[2];
+}
+
+export function removeFileExtension(filename: string) {
+  return filename.substring(0, filename.lastIndexOf(".")) || filename;
+}
+
+export function getPostExcerpt(post: Record<string, any>) {
+  return post.rawContent().substr(0, 255);
+}
+
+export function getPostThumbnail(post: Record<string, any>) {
+  return post.frontmatter.images?.[0];
+}
