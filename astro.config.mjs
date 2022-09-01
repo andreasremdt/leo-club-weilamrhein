fa6b95cb613c30356ac2bc7fa82bf2b84e245db8
Andreas Remdt <me@andreasremdt.com>
2022-09-01 20:30:11 +0200

    chore: remove `@astrojs/mdx`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://leo-club-weilamrhein.de",
-  integrations: [mdx(), sitemap()],
+  integrations: [sitemap()],
 });
