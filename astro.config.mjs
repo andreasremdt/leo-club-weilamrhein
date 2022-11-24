b7534fd70e7d5b6bafd21b86a2a9f52ff445c332
Andreas Remdt <me@andreasremdt.com>
2022-11-24 13:47:21 +0100

    feat: add compression, prettier, remove rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://leo-club-weilamrhein.de",
-  integrations: [sitemap()],
+  integrations: [compress(), sitemap()],
 });
