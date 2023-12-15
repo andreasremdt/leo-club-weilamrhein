5b97e59b5e440800afd3d10263b2ba34f0417c90
Andreas Remdt <me@andreasremdt.com>
2023-12-15 15:12:26 +0100

    fix: update deps, fix issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://leo-club-weilamrhein.de/",
-  integrations: [compress(), sitemap()],
+  integrations: [sitemap()],
 });
