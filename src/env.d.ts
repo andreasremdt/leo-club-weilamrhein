757639c2159ba39abb524885133598f984583f3b
Andreas Remdt <me@andreasremdt.com>
2024-03-01 20:42:55 +0100

    refactor: migrate to astro picture component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module "@fancyapps/ui";
-
-interface ImportMetaEnv {
-  readonly EMAILJS_USER_ID: string;
-  readonly EMAILJS_TEMPLATE_ID: string;
-  readonly EMAILJS_SERVICE_ID: string;
-  readonly EMAILJS_ACCESS_TOKEN: string;
-
-  readonly PUBLIC_ALGOLIA_APPLICATION_ID: string;
-  readonly PUBLIC_ALGOLIA_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
