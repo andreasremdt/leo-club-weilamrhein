cb3768e91bf6a80270f63b74427c8e37b9b2148a
Andreas Remdt <me@andreasremdt.com>
2024-03-01 17:24:13 +0100

    chore: install vercel, change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://leo-club-weilamrhein.de/",
-  integrations: [sitemap(), tailwind()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+  output: "hybrid",
+  image: {
+    domains: ["ik.imagekit.io"],
+  },
+  adapter: vercel(),
 });
