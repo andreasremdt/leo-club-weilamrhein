801946d962ee8059676f88e399288aa7b06e0ad1
Andreas Remdt <me@andreasremdt.com>
2024-03-08 11:21:17 +0100

    fix: change theme color to improve a11y

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,21 @@ export default {
         sans: ["Titillium Web", ...theme.fontFamily.sans],
         serif: ["Lora", ...theme.fontFamily.serif],
       },
+      colors: {
+        primary: {
+          50: "#eef8ff",
+          100: "#dcf0ff",
+          200: "#b2e3ff",
+          300: "#6dcdff",
+          400: "#20b3ff",
+          500: "#009aff",
+          600: "#0079df",
+          700: "#0060b4",
+          800: "#005295",
+          900: "#00437a",
+          950: "#00305d",
+        },
+      },
       boxShadow: {
         upsert: "0 -10px 15px -3px #e7e5e4",
       },
